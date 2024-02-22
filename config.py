dcb5931eb434387ea382ea0b8b5087a1d6f4c7a9
MaryMokretsova <cameron105@mail.ru>
2024-02-22 23:35:47 +0700

    add attach

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ import pydantic
 class Settings(pydantic.BaseModel):
     dotenv.load_dotenv()
 
-    USER_NAME: str = os.getenv('USER_NAME')
-    ACCESS_KEY: str = os.getenv('ACCESS_KEY')
-    remote_url: str = os.getenv('REMOTE_URL')
+    USER_NAME: str = os.environ.get('USER_NAME')
+    ACCESS_KEY: str = os.environ.get('ACCESS_KEY')
+    remote_url: str = os.environ.get('REMOTE_URL')
 
     deviceName: str = 'Samsung Galaxy S23'
     platformVersion: str = '13.0'
